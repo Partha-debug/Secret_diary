3efd1cb63e7aaa33a3b2b59f7a831f674f4ffa55
Partha Sarathi Dash <65017143+Partha-debug@users.noreply.github.com>
2020-10-01 10:25:54 +0530

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+
+    $link = mysqli_connect("sql205.epizy.com","epiz_25813686","jMQJuhRa6Sf4uA","epiz_25813686_secretdi");
+        
+        if (mysqli_connect_error()) {
+            
+            die ("Database Connection Error");
+            
+        }
+
+?>
